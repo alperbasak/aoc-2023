387d69f9ad95ffd56f515944d50fbd2b882cc03a
Alper Basak <alper.basak@imec.be>
2023-12-08 20:31:46 +0100

    4.2

--- src/main/kotlin/four/Card.kt
+++ src/main/kotlin/four/Card.kt
@@ -3,8 +3,10 @@ package four
 import kotlin.math.pow
 
 class Card(private val winningNumbers: List<Int>, private val numbers: List<Int>) {
+    fun count(): Int = numbers.count { winningNumbers.contains(it) }
+
     fun point(): Int {
-        val count = numbers.count { winningNumbers.contains(it) }
+        val count = count()
 
         return if (count == 0) 0 else {
             1 + ((0..(count - 2)).sumOf { 2.0.pow(it.toDouble()).toInt() })
